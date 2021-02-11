33f126930a1618d683be5d1a78eee41ebc5e9e05
Miguel Gueifao <mgueifao@student.42lisboa.com>
2021-02-11 20:11:04 +0000

    Tester fixed to work in the macs

--- test.sh
+++ test.sh
@@ -13,31 +13,34 @@ echo "Running tests:"
 
 cd $pwd/42cursus_gnl_tests
 if [ ! -f ".altered" ]; then
-	sed -i.temp 's/.\/get_next_line/../'  Makefile && rm Makefile.temp
-	sed -i.temp '12 s/./sh&/' Makefile' && rm Makefile.temp
+	sed -i.tmp 's/.\/get_next_line/../'  Makefile && rm Makefile.tmp
+	sed -i.tmp '12 s/\.\//sh &/' Makefile && rm Makefile.tmp
+	sed -i.tmp '16 s/\.\//sh &/' Makefile && rm Makefile.tmp
+	sed -i.tmp '52 s/\.\//sh &/' compile_and_run.sh && rm compile_and_run.sh.tmp
+	sed -i.tmp '60 s/\.\//sh &/' compile_and_run.sh && rm compile_and_run.sh.tmp
 	touch .altered
 fi
 make
 sleep 2
 make bonus
 
-echo -e '\u001b[0m'
+echo -e '\033[0m'
 
 cd $pwd/get-next-line-testing-tools
 if [ ! -f ".altered" ]; then
-	sed -i 's/GNL_SOURCE_DIR = ..\/get_next_line/GNL_SOURCE_DIR = ../' Makefile
-	sed -i 's/int\tdiff_file_size;/int\tdiff_file_size;\n\tchar\tt[11];/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c
-	sed -i 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c
-	sed -i 's/int\tdiff_file_size;/int\tdiff_file_size;\n\tchar\tt[11];/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c
-	sed -i 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c
-	sed -i 's/..\/..\/..\/..\/get_next_line/..\/..\/..\/../' moulitest_files/testframework/v3/Makefile
+	sed -i.tmp 's/GNL_SOURCE_DIR = ..\/get_next_line/GNL_SOURCE_DIR = ../' Makefile && rm Makefile.tmp
+	sed -i.tmp '10 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c && rm moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c.tmp
+	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c && moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c.tmp
+	sed -i.tmp '12 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c && moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c.tmp
+	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c && moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c.tmp
+	sed -i.tmp 's/..\/..\/..\/..\/get_next_line/..\/..\/..\/../' moulitest_files/testframework/v3/Makefile && moulitest_files/testframework/v3/Makefile.tmp
 	touch .altered
 fi
 make mtest_all
 
-echo -e '\u001b[0m'
+echo -e '\033[0m'
 
 cd $pwd/42TESTERS-GNL
 bash ./all_tests_with_bonus.sh
 
-echo -e '\u001b[0m'
+echo -e '\033[0m'
