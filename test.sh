5046e69ab5ffa258d02931a9d79ce57aa67a2cab
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-03-03 20:01:37 +0000

    Refactor script to have a max of 80 characters per line

--- test.sh
+++ test.sh
@@ -4,9 +4,14 @@ pwd=`pwd`
 
 echo "Downloading files:"
 
-[ -d 42cursus_gnl_tests ] || git clone https://github.com/mrjvs/42cursus_gnl_tests.git
-[ -d get-next-line-testing-tools ] || git clone https://github.com/TinfoilPancakes/get-next-line-testing-tools.git
+[ -d 42cursus_gnl_tests ] || git clone https://github.com/mrjvs/42cursus_gnl_te\
+sts.git
+
+[ -d get-next-line-testing-tools ] || git clone https://github.com/TinfoilPanca\
+kes/get-next-line-testing-tools.git
+
 [ -d 42TESTERS-GNL ] || git clone https://github.com/Mazoise/42TESTERS-GNL.git
+
 [ -d gnlTester ] || git clone https://github.com/Tripouille/gnlTester.git
 
 echo "Download complete."
@@ -29,12 +34,28 @@ echo -e '\033[0m'
 
 cd $pwd/get-next-line-testing-tools
 if [ ! -f ".altered" ]; then
-	sed -i.tmp 's/GNL_SOURCE_DIR = ..\/get_next_line/GNL_SOURCE_DIR = ../' Makefile && rm Makefile.tmp
-	sed -i.tmp '10 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c && rm moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c.tmp
-	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c && moulitest_files/get_next_line_tests/tests/41_hard_test_large_file.spec.c.tmp
-	sed -i.tmp '12 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c && moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c.tmp
-	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c && moulitest_files/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c.tmp
-	sed -i.tmp 's/..\/..\/..\/..\/get_next_line/..\/..\/..\/../' moulitest_files/testframework/v3/Makefile && moulitest_files/testframework/v3/Makefile.tmp
+	sed -i.tmp 's/GNL_SOURCE_DIR = ..\/get_next_line/GNL_SOURCE_DIR = ../' Make\
+file && rm Makefile.tmp
+
+	sed -i.tmp '10 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/te\
+sts/41_hard_test_large_file.spec.c && rm moulitest_files/get_next_line_tests/te\
+sts/41_hard_test_large_file.spec.c.tmp
+
+	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/ge\
+t_next_line_tests/tests/41_hard_test_large_file.spec.c && moulitest_files/get_n\
+ext_line_tests/tests/41_hard_test_large_file.spec.c.tmp
+
+	sed -i.tmp '12 s/^/	char	t[11];/' moulitest_files/get_next_line_tests/te\
+sts/42_hard_test_one_big_fat_line.spec.c && moulitest_files/get_next_line_tests\
+/tests/42_hard_test_one_big_fat_line.spec.c.tmp
+
+	sed -i.tmp 's/read(fd3, NULL, 10);/read(fd3, \&t, 10);/' moulitest_files/ge\
+t_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c && moulitest_files\
+/get_next_line_tests/tests/42_hard_test_one_big_fat_line.spec.c.tmp
+
+	sed -i.tmp 's/..\/..\/..\/..\/get_next_line/..\/..\/..\/../' moulitest_file\
+s/testframework/v3/Makefile && moulitest_files/testframework/v3/Makefile.tmp
+
 	touch .altered
 fi
 make mtest_all
@@ -48,3 +69,5 @@ echo -e '\033[0m'
 
 cd $pwd/gnlTester
 make a
+
+echo -e '\033[0m'
